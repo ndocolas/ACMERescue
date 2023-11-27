9e6a1fb8db7d7de770f9ca33150700d8046b12c8
salocod <nicolas.docolas1804@gmail.com>
2023-11-27 19:19:46 -0300

    trabalho final

--- main/src/main/java/Main.java
+++ main/src/main/java/Main.java
@@ -1,10 +1,8 @@
 import Manage.ACMERescue;
 import Manage.LeituraInicial;
-import Manage.TelaPrincipalGUI;
 
 public class Main {
     public static void main(String[] args) {
-        //new LeituraInicial(new ACMERescue());
-        new TelaPrincipalGUI(new ACMERescue());
+        new LeituraInicial(new ACMERescue());
     }
 }
