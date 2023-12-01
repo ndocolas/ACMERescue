e4f144d706d8359240b766c119388cffe59cf303
salocod <nicolas.docolas1804@gmail.com>
2023-12-01 16:35:11 -0300

    alteracao estrutura

--- main/src/main/java/Main.java
+++ main/src/main/java/Main.java
@@ -3,6 +3,6 @@ import Manage.LeituraInicial;
 
 public class Main {
     public static void main(String[] args) {
-        new LeituraInicial(new ACMERescue());//teste
+        new LeituraInicial(new ACMERescue());
     }
 }
